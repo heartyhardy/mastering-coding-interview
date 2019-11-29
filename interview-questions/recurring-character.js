d633270fb3c0cf85dfbe4187ebd4643b698e8a02
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-11-29 21:22:19 +0530

    Added XOR solution to find duplicates

--- interview-questions/recurring-character.js
+++ interview-questions/recurring-character.js
@@ -1,9 +1,4 @@
 const findDuplicate = arr => {
-    // Return if arr.len < 2
-    // Convert char to ascii -> int
-    // Use XOR operator
-    // num xor num = 0
-    // num xor 0 = num
     let obj = {}
     for(let i=0;i< arr.length; i++){
         if(obj[arr[i]] !== undefined){
@@ -12,9 +7,35 @@ const findDuplicate = arr => {
             obj[arr[i]] = i
         }
     }
-    return null
-    
+    return null    
 }
 
-let x = findDuplicate([11, 12, 9, 10, 8, 9, 20])
+const findDuplicate2 = arr => {
+    let map = new Map()
+    for(let i=0;i< arr.length; i++){
+        if(map.has(arr[i])){
+            return [map.get(arr[i]),i]
+        } else {
+            map.set(arr[i],i)
+        }
+    }
+    return null    
+}
+
+const findDuplicate3 = arr => {
+    // This has to be a sorted array for this to work
+    res = arr[0]
+    for(let i=1; i<arr.length;i++){
+        res = res ^ arr[i]        
+        if(res === 0){
+            return [i-1,i]
+        }else{
+            res = arr[i]
+        }
+    }
+    return []
+}
+
+// let x = findDuplicate2([11, 12, 9, 10, 8, 9, 20])
+let x = findDuplicate3([2,3,4,5,6,7,7])
 console.log(x)
